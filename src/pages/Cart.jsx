3126a468f39834a2e051044e6fa47b1f14a026d3
Satyendra Mourya <66292112+niraj202@users.noreply.github.com>
2023-09-19 15:43:07 +0530

    added bisa card demo data

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -44,11 +44,11 @@ const Cart = () => {
         console.error('Payment error:', error);
         toast.error('Payment failed. Please try again.');
       }
-      
+
     } else {
       toast.error('Please login first');
     }
-    
+
   }
 
   return (
@@ -60,14 +60,27 @@ const Cart = () => {
         <div className="lg:w-1/3 bg-[#fafafa] py-6 px-4">
           <div className="flex flex-col gap-6 border-b-[1px] border-b-gray-400 pb-6">
             <h2 className="text-2xl font-medium">Cart totals</h2>
-            <p className="flex items-center gap-4 text-base">
-              Subtotal <span className="font-titlFont font-bold text-lg">$ {totalAmount}</span>
-            </p>
+
+            <div>
+              <p className="flex items-center gap-4 text-base">
+                Subtotal <span className="font-titlFont font-bold text-lg">$ {totalAmount}</span>
+              </p>
+            </div>
             <p className="flex items-start gap-4 text-base">Shiping <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi hic.</span></p>
           </div>
           <p className="font-titlFont font-semibold flex justify-between mt-6"> Total <span className="text-xl font-bold">$ {totalAmount}</span></p>
-          <button onClick={handleCheckout} className=" active:scale-95 text-base bg-black text-white w-full py-3 mt-6 hover:bg-gray-800 duration-500">proceed to checkout</button>
+          <button onClick={handleCheckout} className=" active:scale-95 text-base bg-black text-white w-full py-3 mt-6 hover:bg-gray-800 duration-500" target='_blank' >proceed to checkout</button>
+
+
+          <div className="flex flex-col gap-4 border-y-[1px] mt-4 border-y-gray-400 pb-6 text-sm">
+            <h2 className=" font-medium">Demo Data</h2>
+            <p className="flex items-center gap-4">
+              Card info. <span className="font-titlFont font-bold text-lg"> 4111 1111 1111 1111</span>
+            </p>
+            <p className="flex items-start gap-4">Billing add- country <span>United States</span></p>
+          </div>
         </div>
+
       </div>
       <ToastContainer
         position="top-right"
